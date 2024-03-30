9bc634576286e600552fc46d291e55c09236140d
julius <nyerereoffice@gmail.com>
2024-03-30 05:46:28 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+let secondsElapsed = 0;
+let interval = null;
+const time = document.getElementById('time')
+
+
+function padStart(){
+    return String(value).padStart(2, "0")
+}
+
+
+function setTime(){
+    const minutes = Math.floor(secondsElapsed / 60)
+    const seconds = secondsElapsed % 60
+    time.innerHTML = `${minutes}:${seconds}`
+}
+
+
+function timer(){
+    secondsElapsed ++;
+    setTime()
+}
+
+
+function startClock(){
+    interval = setInterval(timer, 1000)
+}
+
+function stopClock(){
+
+}
+
+function resetClock(){
+
+}
